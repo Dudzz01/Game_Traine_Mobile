ab3343b89183f4480dd5dfa79fe524b69b5e96f9
Eduardo Mury Assafin <101987520+Dudzz01@users.noreply.github.com>
2022-07-07 04:27:27 -0300

    update tiro aprimorado

--- Assets/Scripts/ScriptsEnemies/Enemy_Bullet.cs
+++ Assets/Scripts/ScriptsEnemies/Enemy_Bullet.cs
@@ -6,29 +6,38 @@ public class Enemy_Bullet : MonoBehaviour
 {
     
     public BoxCollider2D box_bullet; // mascara de colisao do tiro do inimigo estático
+    public Rigidbody2D rig; // rigidbody da bullet
 
     // Start is called before the first frame update
-    void Start()
+   /* void Start()
     {
         
-    }
+    }*/
 
     // Update is called once per frame
-    void Update()
+   /* void Update()
     {
         
     }
+*/
+    void FixedUpdate() 
+    {
+       rig.AddForce(new Vector2(0,-1.32f),ForceMode2D.Impulse); // impulso do tiro do inimigo ir para baixo
+    }
+       
 
-         void OnTriggerEnter2D(Collider2D col) // metodo para fazer o efeito de tiro, o tiro ultrapassa o inimigo dando um efeito de que o tiro sai do inimigo, e aos demais objetos do jogo ele realmente colide sem ultrapassar
+       
+   void OnTriggerEnter2D(Collider2D col) // metodo para fazer o efeito de tiro, o tiro ultrapassa o inimigo dando um efeito de que o tiro sai do inimigo, e aos demais objetos do jogo ele realmente colide sem ultrapassar
+     {
+        if(col.CompareTag("Enemy"))
         {
-           if(col.CompareTag("Enemy"))
-           {
-             box_bullet.isTrigger = true;
-           }
-           else
-           {
-             box_bullet.isTrigger = false;
-           }
+           box_bullet.isTrigger = true;
+        }
+        else
+        {
+          box_bullet.isTrigger = false;
+          Destroy(this.gameObject); // possivel mudancas nessa estrutura com implementacoes de mais condicoes
+        }
         }
         
 
