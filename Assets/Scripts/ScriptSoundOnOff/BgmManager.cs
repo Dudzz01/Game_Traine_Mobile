f63f072cf934c5c61ddbecfd339f4645bfb0b797
Eduardo Mury Assafin <101987520+Dudzz01@users.noreply.github.com>
2022-07-29 22:47:03 -0300

    Update CORRIGINDO BUG MUSICA

--- Assets/Scripts/ScriptSoundOnOff/BgmManager.cs
+++ Assets/Scripts/ScriptSoundOnOff/BgmManager.cs
@@ -13,13 +13,13 @@ public class BgmManager : MonoBehaviour
         if(instance == null)//Se nao existe nenhum objeto da cena que tenha a classe referenciada 
         {
             instance = this;//A gente referencia a classe
-            DontDestroyOnLoad(instance);//E não permite que esse objeto seja destruido entre cenas
+            DontDestroyOnLoad(instance);//E nï¿½o permite que esse objeto seja destruido entre cenas
             AS = GetComponent<AudioSource>();
             StartCoroutine("StartLoop");
         }
         else//Se ja existe essa classe referenciada
         {
-            Destroy(gameObject);//a gente Destroy o gameObject, pois já existe um deles na cena 
+            Destroy(gameObject);//a gente Destroy o gameObject, pois jï¿½ existe um deles na cena 
         }
         //if e else necessarios pra garantir que nunca tenham 2 ou + obj tocando a mesma musica
     }
@@ -27,7 +27,7 @@ public class BgmManager : MonoBehaviour
    IEnumerator StartLoop()
     {
         float TimeToWait = BgmIntro.length;
-        yield return new WaitForSeconds(TimeToWait - 1f);
+        yield return new WaitForSeconds(TimeToWait);
         AS.loop = true;
         AS.clip = BgmLoop;
         
